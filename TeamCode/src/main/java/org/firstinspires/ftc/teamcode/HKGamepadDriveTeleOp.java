955965c1888eb4f092a89b5a9643f224a16f6919
APCODEZ <71616101+testUser453@users.noreply.github.com>
2022-08-01 19:43:02 -0400

    edited files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HKGamepadDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HKGamepadDriveTeleOp.java
@@ -14,6 +14,8 @@ public class HKGamepadDriveTeleOp extends OpMode {
     double leftWheelPower;
     double rightWheelPower;
     double servoControl;
+    double armControl;
+    double clawControl;
 
 
     @Override
