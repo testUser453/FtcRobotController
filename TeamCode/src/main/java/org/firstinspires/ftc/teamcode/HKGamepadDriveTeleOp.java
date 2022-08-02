e1162b922879b77964cc9aebae488cde9d5fa9cb
APCODEZ <71616101+testUser453@users.noreply.github.com>
2022-08-02 17:33:19 -0400

    HK added update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HKGamepadDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HKGamepadDriveTeleOp.java
@@ -5,23 +5,26 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="Gamepad Drive", group="Training")
+@TeleOp(name="HK Drive", group="Training")
 public class HKGamepadDriveTeleOp extends OpMode {
 
     DcMotor leftWheel;
     DcMotor rightWheel;
+    DcMotor armControl;
+
 
     double leftWheelPower;
     double rightWheelPower;
-    double servoControl;
-    double armControl;
-    double clawControl;
+    double armControlDetect;
+
 
 
     @Override
     public void init() {
         leftWheel = hardwareMap.dcMotor.get("leftMotor");
         rightWheel = hardwareMap.dcMotor.get("rightMotor");
+        armControl = hardwareMap.dcMotor.get("arm");
+
 
         leftWheel.setDirection(DcMotor.Direction.REVERSE);
 
@@ -29,12 +32,24 @@ public class HKGamepadDriveTeleOp extends OpMode {
 
     @Override
     public void loop() {
-        leftWheelPower = gamepad1.left_stick_y;
-        rightWheelPower = gamepad1.right_stick_y;
+        leftWheelPower = -gamepad1.left_stick_y;
+        rightWheelPower = -gamepad1.right_stick_y;
 
 
         leftWheel.setPower(leftWheelPower);
         rightWheel.setPower(rightWheelPower);
+
+        if (gamepad1.a) {
+            armControl.setPower(armControlDetect);
+
+        } else if (gamepad1.x) {
+            armControl.setPower(-armControlDetect);
+
+
+
+
+        }
+
 
 
     }
