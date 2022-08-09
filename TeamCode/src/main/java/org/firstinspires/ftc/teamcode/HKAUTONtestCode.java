4449f47379a457e3068ebb1a13b8e2486343d89b
APCODEZ <71616101+testUser453@users.noreply.github.com>
2022-08-09 10:21:09 -0400

    Create HKAUTONtestCode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HKAUTONtestCode.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name="HKAUTONtest", group="HK")
+public class HKAUTONtestCode extends LinearOpMode {
+
+    DcMotor leftWheel;
+    DcMotor rightWheel;
+    DcMotor armMotor;
+    Servo clawServo;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftWheel = hardwareMap.dcMotor.get("leftMotor");
+        rightWheel = hardwareMap.dcMotor.get("rightMotor");
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
+        armMotor = hardwareMap.dcMotor.get("arm");
+        clawServo = hardwareMap.servo.get("claw");
+
+        armMotor.setPower(0);
+
+        waitForStart();
+
+        //go forward at .5 speed for 2 seconds
+
+        leftWheel.setPower(.5);
+        rightWheel.setPower(.5);
+
+        sleep(2000);
+
+        //turn right for half a second at .5 speed
+
+        leftWheel.setPower(.5);
+        rightWheel.setPower(-.5);
+
+        sleep(500);
+
+        //move forward for 1/5th of a second
+
+        leftWheel.setPower(1);
+        rightWheel.setPower(1);
+
+        sleep(200);
+
+        // armMotor goes up at a power of .3 for 1/5th of a second
+        armMotor.setPower(.3);
+
+        sleep(200);
+
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftWheel.setPower(.5);
+        rightWheel.setPower(.5);
+
+        sleep(100);
+
+        armMotor.setPower(0);
+
+
+
+
+
+
+
+
+
+
+    }
+}
